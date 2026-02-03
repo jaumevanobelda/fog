60c14a7a77e3084045e23a8bf04c1d32c4d47f13
jaume <jaumevab@gmail.com>
2026-02-03 18:36:39 +0100

    params slug games

--- backendRubyOnRails/games/app/controllers/game_controller.rb
+++ backendRubyOnRails/games/app/controllers/game_controller.rb
@@ -30,7 +30,7 @@ class GameController < ApplicationController
 
     def update
         if Game.find_by_slug(params[:game][:nom].parameterize)
-            return render json: { error: "Ya existe un juego con ese nombre" },status: :conflict if params[:game][:nom].parameterize != params[:id]
+            return render json: { error: "Ya existe un juego con ese nombre" },status: :conflict if params[:game][:nom].parameterize != params[:slug]
         end
         if @game.update(game_params)
             @game.category_ids = Category.where(slug: params[:game][:categories]).pluck(:id)
@@ -54,7 +54,7 @@ class GameController < ApplicationController
     end
 
     def set_game
-        @game = Game.find_by_slug(params[:id])
+        @game = Game.find_by_slug(params[:slug])
         return render json: { error: "No se ha encontrado el juego" } if @game == nil
         
         if request.headers["User-Role"] == "DEVELOPER"
--- backendRubyOnRails/games/config/routes.rb
+++ backendRubyOnRails/games/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :game
+  resources :game,param: :slug
+  # scope :game do
+	# 	get 'test', to: 'game#index'
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
